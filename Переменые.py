6f1a6220050aa240ae789b72f9e2c08c6545a6e5
PavelVladivostok <zihvav@gmail.com>
2024-10-08 21:45:03 +1000

    Add files via upload

---
+++ Переменые.py
@@ -0,0 +1,9 @@
+#  «Переменные»
+
+
+Number_of_completed_dz=12 #Количество выполненных ДЗ
+Number_of_hours_spent=1.5 #Количество затраченных часов
+Time_for_one_task =1/4 # среднее время выполнения
+Course_name ='pathon:'  #Названия проэкта
+# решения задания 
+print (Course_name,'number_of_completed_dz:',Number_of_completed_dz,'Number_of_hours_spent:',Number_of_hours_spent, 'Time_for_one_task:',Time_for_one_task )
